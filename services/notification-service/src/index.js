651ef55fbe896b40ab2995996da0338f3a23c057
Aleksandar Radovanovic <radovanovica1993@gmail.com>
2025-06-14 18:12:12 +0200

    [Notification service[ - Fixes related to emitted events

--- services/notification-service/src/index.js
+++ services/notification-service/src/index.js
@@ -24,23 +24,23 @@ async function startKafka() {
     eachMessage: async ({ topic, message }) => {
       const data = JSON.parse(message.value.toString());
       console.log(`Received event [${topic}] ->`, data);
-
+      
       // Simulated notification behavior
       switch (topic) {
         case 'order.created':
-          mockSendEmail(`Order ${data.orderId || 'X'} created successfully.`);
+          mockSendEmail(`Order ${data?.cart?.id || 'X'} created successfully.`);
           break;
         case 'stock.reserved':
-          mockSendEmail(`Stock reserved for Order ${data.orderId}.`);
+          mockSendEmail(`Stock reserved for Order ${data?.order?.cart?.id}.`);
           break;
         case 'stock.failed':
-          mockSendEmail(`Stock reservation failed: ${data.reason}`);
+          mockSendEmail(`Stock reservation failed for Order ${data?.order?.cart?.id} with reason: ${data.reason}`);
           break;
         case 'payment.authorized':
           mockSendEmail(`Payment authorized for Order ${data.orderId}.`);
           break;
         case 'payment.failed':
-          mockSendEmail(`Payment failed: ${data.reason}`);
+          mockSendEmail(`Payment failed for Order ${data.orderId} with reason: ${data.reason}.`);
           break;
         default:
           console.log('Unknown event type:', topic);
