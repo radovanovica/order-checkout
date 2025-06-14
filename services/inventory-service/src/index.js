c521c1cb687672efc1eec6adbde70cd33632a972
Aleksandar Radovanovic <radovanovica1993@gmail.com>
2025-06-14 18:11:49 +0200

    [Inventory service] - Listen for order.crated and emit stock* events

--- services/inventory-service/src/index.js
+++ services/inventory-service/src/index.js
@@ -1,10 +1,45 @@
+// inventory-service/src/index.js
 const express = require('express');
-const app = express();
+const { emitKafkaEvent } = require('common-libs/kafka/producer');
+const { createKafkaConsumer } = require('common-libs/kafka/consumer');
 
-app.get('/', (req, res) => {
-  res.send('Inventory Service');
+const app = express();
+const PORT = process.env.PORT || 3005;
+
+// Mock function to reserve stock
+async function reserveStock(order) {
+  const allInStock = true
+  if (!allInStock) {
+    await emitKafkaEvent('stock.failed', {
+      order: order,
+      reason: 'Insufficient stock'
+    });
+  } else {
+    await emitKafkaEvent('stock.reserved', {
+      order:order
+    });
+  }
+}
+
+// Start listening to order.created events
+async function startKafkaConsumer() {
+  await createKafkaConsumer({
+    groupId: 'inventory-group',
+    topic: 'order.created',
+    handleMessage: async (order) => {
+      console.log('Received order.created event:', order);
+      console.log('Received order.created event:', order?.cart?.items?.[0]?.price);
+      await reserveStock(order);
+    }
+  });
+}
+
+// Health check
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
 });
 
-app.listen(3002, () => {
-  console.log('Inventory Service running on port 3002');
+app.listen(PORT, () => {
+  console.log(`Inventory service running on port ${PORT}`);
+  startKafkaConsumer();
 });
