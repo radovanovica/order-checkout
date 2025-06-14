27fdab50e6f419afdce8eb046b8d0695c2954d9c
Aleksandar Radovanovic <radovanovica1993@gmail.com>
2025-06-14 18:17:20 +0200

    [Checkout service] - API layer; Call request validation functions; Emit order.created event;

--- services/checkout-service/src/index.js
+++ services/checkout-service/src/index.js
@@ -1,10 +1,54 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const { emitKafkaEvent } = require('common-libs/kafka/producer');
+const validateCart = require('./services/ValidationClient');
+const schema = require('./services/schema');
 
-app.get('/', (req, res) => {
-  res.send('Checkout Service');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+async function validateJsonInput(input) {
+  try {
+    await schema.validate(input);
+    return {
+      valid: true,
+      message: 'Input is valid'
+    };
+  } catch (err) {
+    return {
+      valid: false,
+      message: err.message
+    };
+  }
+}
+// Checkout endpoint
+app.post('/checkout', async (req, res) => {
+  const cart = req.body;
+  cart.id = Date.now(); // Generate a unique ID for the cart
+  try {
+    // Validate request data - yup schema validation
+    const requestValidationResult = await validateJsonInput(cart);
+    // Validate cart items and promo codes
+    const validationResult = await validateCart(cart);
+    if (!validationResult.valid || !requestValidationResult?.valid) {
+      return res.status(400).json({ error: validationResult.message });
+    }
+
+    // Emit event to reserve stock
+    await emitKafkaEvent('order.created', { cart });
+
+    res.status(202).json({ message: 'Stock reservation started' });
+  } catch (error) {
+    console.error('Checkout error:', error);
+    res.status(500).json({ error: 'Checkout failed' });
+  }
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Checkout Service running on port 3000');
 });
